2a390278d846e011406775b3bc9df144ae0723d5
Josh Iden <iden.josh@gmail.com>
2023-01-15 18:19:36 -0500

    update scripts

--- Mode/WORKSHEET.sql
+++ Mode/WORKSHEET.sql
@@ -71,3 +71,16 @@ ON a.company_permalink = c.permalink
 WHERE c.founded_at_clean IS NOT NULL
 GROUP BY 1
 ORDER BY 5 DESC;
+
+/*
+Write a query that separates the `location` field into separate fields for
+latitude and longitude. You can compare your results against the actual
+`lat` and `lon` fields in the table.
+*/
+
+SELECT location,
+       TRIM(leading '(' FROM LEFT(location, POSITION(',' IN location) - 1))
+       AS lattitude,
+       TRIM(trailing ')' FROM RIGHT(location, LENGTH(location) -
+       POSITION(',' IN location) ) ) AS longitude
+  FROM tutorial.sf_crime_incidents_2014_01;
