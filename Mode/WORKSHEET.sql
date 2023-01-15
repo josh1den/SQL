f9f90eeac9ab5c197f089e5fb9e7aa47a1312734
Josh Iden <iden.josh@gmail.com>
2023-01-15 12:13:39 -0500

    update worksheet

--- Mode/WORKSHEET.sql
+++ Mode/WORKSHEET.sql
@@ -39,3 +39,35 @@ JOIN tutorial.crunchbase_companies c
 ON t.company_permalink = c.permalink
 GROUP BY 1, 2
 ORDER BY 3 DESC;
+
+/*
+Convert the funding_total_usd and founded_at_clean columns in the
+tutorial.crunchbase_companies_clean_date table to strings (varchar)
+using a different formatting function for each one
+*/
+
+SELECT funding_total_usd :: VARCHAR,
+       CAST(founded_at_clean AS VARCHAR)
+FROM tutorial.crunchbase_companies_clean_date;
+
+/*
+Write a query that counts the number of companies acquired within
+3 years, 5 years, and 10 years of being founded (in 3 separate
+columns). Include a column for total companies acquired as well.
+Group by category and limit to only rows with a founding date.
+*/
+
+SELECT c.category_code,
+       COUNT(CASE WHEN a.acquired_at_cleaned <= c.founded_at_clean::timestamp +
+             INTERVAL '3 years' THEN 1 ELSE NULL END) AS aqcuired_3_yrs,
+       COUNT(CASE WHEN a.acquired_at_cleaned <= c.founded_at_clean::timestamp +
+             INTERVAL '5 years' THEN 1 ELSE NULL END) AS acquired_5_yrs,
+       COUNT(CASE WHEN a.acquired_at_cleaned <= c.founded_at_clean::timestamp +
+             INTERVAL '10 years' THEN 1 ELSE NULL END) AS acquired_10_yrs,
+       COUNT(1) AS total
+FROM tutorial.crunchbase_companies_clean_date c
+JOIN tutorial.crunchbase_acquisitions_clean_date a
+ON a.company_permalink = c.permalink
+WHERE c.founded_at_clean IS NOT NULL
+GROUP BY 1
+ORDER BY 5 DESC;
